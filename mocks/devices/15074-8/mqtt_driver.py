6672f6b29edbf8323ae21fe82b825a1ff799f409
Iffern <kicajka1999@gmail.com>
2021-01-27 22:53:12 +0100

    changed mqtt server

--- mocks/devices/15074-8/mqtt_driver.py
+++ mocks/devices/15074-8/mqtt_driver.py
@@ -88,9 +88,9 @@ def send_setting_patient_confirm():
     client.publish(topics["send_setting_patient_confirm"], "OK", 0, False)
 
 
-def send_measurement(measurement, unit):
-    print("sending " + topics["send_measurement"] + " " + str(measurement) + "#" + str(unit))
-    client.publish(topics["send_measurement"], str(measurement) + "#" + str(unit), 0, False)
+def send_measurement(measurement):
+    print("sending " + topics["send_measurement"] + " " + measurement)
+    client.publish(topics["send_measurement"], measurement, 0, False)
 
 
 def send_unpair_request():
@@ -176,6 +176,6 @@ def unsubscribe_all_topics():
 
 def run_mqtt_in_background():
     global client
-    client.connect("127.0.0.1", 1883, 60)
+    client.connect("broker.hivemq.com", 1883, 60)
     client.on_message = on_message
     client.loop_forever()
